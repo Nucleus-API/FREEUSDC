43dedc74e64c5c42fe321d672d5b03c3715a9aa4
Nikhil Kulkarni <nikhil896@gmail.com>
2022-11-05 10:35:48 -0700

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  Box,
-  Grid,
-  theme,
-  GridItem,
-} from "@chakra-ui/react"
+import * as React from "react";
+
+import { Box, ChakraProvider, Grid, GridItem, theme } from "@chakra-ui/react";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box backgroundColor='black' h='100vh'>
-      <Grid
-        templateColumns='200px 4fr 4fr'
-        templateAreas={`"logo card transactions"`}
-      >
-        <GridItem minW='200px' h='100vh' bg='white' area={'logo'} />
-        <GridItem minW='200px' h='100vh' bg='tomato' area={'card'} />
-        <GridItem minW='200px' h='100vh' bg='papayawhip' area={'transactions'}/>
+    <Box backgroundColor="black" h="100vh">
+      <Grid templateColumns="200px 4fr 4fr" templateAreas={`"logo card transactions"`}>
+        <GridItem minW="200px" h="100vh" bg="white" area={"logo"} />
+        <GridItem minW="200px" h="100vh" bg="tomato" area={"card"} />
+        <GridItem minW="200px" h="100vh" bg="papayawhip" area={"transactions"} />
       </Grid>
     </Box>
   </ChakraProvider>
-)
+);
