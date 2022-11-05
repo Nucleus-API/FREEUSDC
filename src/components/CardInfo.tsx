da7b28a216d3311255a50944312fac6c3b77a9b3
Nikhil Kulkarni <nikhil896@gmail.com>
2022-11-05 11:57:12 -0700

    more info

--- src/components/CardInfo.tsx
+++ src/components/CardInfo.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Text, VStack, useDisclosure } from "@chakra-ui/react";
+import { Box, Button, HStack, Image, Text, VStack, useDisclosure } from "@chakra-ui/react";
 
 import { KycModal } from "./onboarding/KycModal";
 
@@ -8,14 +8,58 @@ export const CardInfo = () => {
   return (
     <VStack w="full" h="100%" borderColor="white" borderWidth={6} borderRadius={36} flexGrow={1}>
       <VStack w="full" h="full" alignItems="flex-start" spacing={12} pt={6} pb={6} pr={10} pl={10}>
-        <Box flexGrow={1}>
+        <Box>
           <Text fontFamily="Roboto Mono" fontWeight="bold" color="white" fontSize={32}>
             HUNDREDS OF CARDS, ONE WALLET: <br />
             SPEND IT IF YOU'RE FAST ENOUGH
           </Text>
         </Box>
+
+        <Text fontWeight="bold" fontSize={20} color="white" flexGrow={1}>
+          FreeUSDC is a{" "}
+          <a
+            href="https://etherscan.io/address/0x00a625febc1b7e8aa2b267d906de0b3a1a2f8be8"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{
+              color: "#00D1FF",
+              fontWeight: "bold",
+              textDecoration: "underline",
+            }}
+          >
+            shared wallet
+          </a>
+          . USDC is deposited at random times. <br />
+          <br />
+          All cards are connected to the same wallet. Race to spend it before others.
+        </Text>
+
+        <VStack spacing={4} alignItems="flex-start">
+          <HStack spacing={8}>
+            <Image src="./01x.png" w={20}></Image>
+            <Text fontFamily="Roboto Mono" fontSize={40} fontWeight="bold" color="white">
+              GET A CARD
+            </Text>
+          </HStack>
+
+          <HStack spacing={8}>
+            <Image src="./0x2.png" w={20}></Image>
+            <Text fontFamily="Roboto Mono" fontSize={40} fontWeight="bold" color="white">
+              WAIT FOR THE DROP
+            </Text>
+          </HStack>
+
+          <HStack spacing={8}>
+            <Image src="./0x3.png" w={20}></Image>
+            <Text fontFamily="Roboto Mono" fontSize={40} fontWeight="bold" color="white">
+              SPEND THE MONEY
+            </Text>
+          </HStack>
+        </VStack>
+
         <Button
           onClick={() => onKycOpen()}
+          fontFamily="Roboto Mono"
           w="100%"
           bg="#8526FE"
           mb="20px"
